4f1320e492e1a2dc6e1b08fbf1aa8a22b8867ba8
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-04-01 17:17:11 -0300

    Adicionando Particoes ao Projeto2

--- src/connection/ConexaoERP.py
+++ src/connection/ConexaoERP.py
@@ -1,31 +1,40 @@
 import jaydebeapi
 from contextlib import contextmanager
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
 import os
 
 
 @contextmanager
 def ConexaoInternoMPL():
-    conn = None
-    load_dotenv("C:\Users\luis.fernando\Desktop\PROJETOS MPL\ModuloGestaoMetasIndustrialMPL\ambiente.env")
+    """ Gerencia a conexão com o banco de dados usando JayDeBeApi """
+
+    env_path = r"C:\Users\luis.fernando\Desktop\PROJETOS MPL\ModuloGestaoMetasIndustrialMPL\_ambiente.env"
+    # Carregar variáveis de ambiente do arquivo .env
+    load_dotenv(env_path)
+
+    # Obter valores das variáveis
+    caminho_absoluto = os.getenv('CAMINHO')
     user = os.getenv('CSW_USER')
     password = os.getenv('CSW_PASSWORD')
     host = os.getenv('CSW_HOST')
+
+    # Verificar se variáveis foram carregadas corretamente
+    if not all([caminho_absoluto, user, password, host]):
+        raise ValueError("Erro: Variáveis de ambiente ausentes ou não carregadas corretamente.")
+
+    conn = None  # Inicializa a variável antes do bloco try
 
     try:
         conn = jaydebeapi.connect(
             'com.intersys.jdbc.CacheDriver',
             f'jdbc:Cache://{host}:1972/CONSISTEM',
-            {'user': f'{user}', 'password': f'{password}'},
-            f'{caminhoAbsoluto}/src/connection/CacheDB.jar'
+            {'user': user, 'password': password},
+            f'{caminho_absoluto}/src/connection/CacheDB.jar'
         )
         yield conn
     finally:
-        if conn is not None:
+        if conn:  # Verifica se a conexão foi estabelecida antes de tentar fechar
             conn.close()
 
 
-load_dotenv('./var_ambiente.env')
-host = os.getenv('CSW_HOST')
 
-print(host)
