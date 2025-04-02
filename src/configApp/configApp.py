78519e83a353aa9fcc7dbd5939e99ba5515252a4
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-04-02 15:07:54 -0300

    atualizando requirements.txt

--- src/configApp/configApp.py
+++ src/configApp/configApp.py
@@ -1,4 +1,4 @@
 
 '''ARQUIVO DE CONFIGURACAO DE VARIAVEIS GLOBAIS DO PROJETO PYTHON'''
 
-localProjeto = r"C:\Users\luis.fernando\Desktop\PROJETOS MPL\ModuloGestaoMetasIndustrialMPL\_ambiente.env"
+localProjeto = "/home/grupompl/GestaoIndustrial"
