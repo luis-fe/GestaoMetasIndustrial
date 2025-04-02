c22b642280c88fb8b6e3347bd92f7245784ef44f
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-04-02 14:24:34 -0300

    atualizando requirements.txt

--- app_run.py
+++ app_run.py
@@ -1,13 +1,12 @@
 from flask import Flask
 import os
-from routes import routes_blueprint
+from routes import routes_blueprint  # Certifique-se de que 'routes.py' está na mesma pasta
 
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 5001))
 
-
+# Registrar o Blueprint corretamente
 app.register_blueprint(routes_blueprint)
 
 if __name__ == '__main__':
-
     app.run(host='0.0.0.0', port=port)
