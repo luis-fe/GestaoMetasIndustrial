f0557313f6059cc9c27d5a429b9cfcbd50719ea5
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-04-02 15:13:16 -0300

    atualizando requirements.txt

--- app_run.py
+++ app_run.py
@@ -1,6 +1,9 @@
 from flask import Flask
 import os
+
+from src.configApp import configApp
 from src.routes import routes_blueprint  # Certifique-se de que 'routes.py' está na mesma pasta
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 5000))
@@ -9,5 +12,11 @@ port = int(os.environ.get('PORT', 5000))
 app.register_blueprint(routes_blueprint)
 
 if __name__ == '__main__':
-    print()
+
+    env_path = configApp.localProjeto
+    # Carregar variáveis de ambiente do arquivo .env
+    load_dotenv(f'{env_path}/_ambiente.env')
+    db_name = os.getenv('POSTGRES_DB')
+
+    print(db_name)
     app.run(host='0.0.0.0', port=port)
--- src/connection/ConexaoERP.py
+++ src/connection/ConexaoERP.py
@@ -15,13 +15,12 @@ def ConexaoInternoMPL():
     load_dotenv(f'{env_path}/_ambiente.env')
 
     # Obter valores das variáveis
-    caminho_absoluto = os.getenv('CAMINHO')
     user = os.getenv('CSW_USER')
     password = os.getenv('CSW_PASSWORD')
     host = os.getenv('CSW_HOST')
 
     # Verificar se variáveis foram carregadas corretamente
-    if not all([caminho_absoluto, user, password, host]):
+    if not all([user, password, host]):
         raise ValueError("Erro: Variáveis de ambiente ausentes ou não carregadas corretamente.")
 
     conn = None  # Inicializa a variável antes do bloco try
@@ -31,7 +30,7 @@ def ConexaoInternoMPL():
             'com.intersys.jdbc.CacheDriver',
             f'jdbc:Cache://{host}:1972/CONSISTEM',
             {'user': user, 'password': password},
-            f'{caminho_absoluto}/src/connection/CacheDB.jar'
+            f'{env_path}/src/connection/CacheDB.jar'
         )
         yield conn
     finally:
