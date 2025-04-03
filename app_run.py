8911be39b60717a3da4f75ef22b9c54b22c17242
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-04-03 14:54:05 -0300

    escolha da porta

--- app_run.py
+++ app_run.py
@@ -6,11 +6,18 @@ from src.routes import routes_blueprint  # Certifique-se de que 'routes.py' est√
 from dotenv import load_dotenv
 
 app = Flask(__name__)
-port = int(os.environ.get('PORT', 5000))
+env_path = configApp.localProjeto
+# Carregar vari√°veis de ambiente do arquivo .env
+load_dotenv(f'{env_path}/_ambiente.env')
+porta_escolhida = os.getenv('PORTA_APLICACAO')
+
+port = int(os.environ.get('PORT', int(porta_escolhida)))
 
 # Registrar o Blueprint corretamente
 app.register_blueprint(routes_blueprint)
 
 if __name__ == '__main__':
+
+
 
     app.run(host='0.0.0.0', port=port)
