557a5e6ba6ae081c62a90c918f2b998030de8d4b
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-05-06 17:58:05 -0300

    implemando o resumo de fila por fase

--- src/models/OrdemProd.py
+++ src/models/OrdemProd.py
@@ -180,7 +180,7 @@ class OrdemProd():
             # 7 - buscando a qtde de peças da OP
             sqlBuscarPecas = self.ordemProd_geral()
             fila = pd.merge(fila, sqlBuscarPecas, on='numeroOP', how='left')
-            fila['pcs'].fillna('-',inplace=True)
+            fila['pcs'].fillna(0,inplace=True)
             fila.fillna('-',inplace=True)
             # 8 - acrescentando a colecao e o ano de cada Ordem de Producao
             fila['COLECAO'] = fila['desLote'].apply(self.__tratamentoInformacaoColecao)
