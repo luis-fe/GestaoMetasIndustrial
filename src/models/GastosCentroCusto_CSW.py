7ea0a2d2843953fdf00ff015ff691ca48d254e7d
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-07-26 10:09:51 -0300

    Atualizando os dados para testar o usuo do in "ano"

--- src/models/GastosCentroCusto_CSW.py
+++ src/models/GastosCentroCusto_CSW.py
@@ -296,8 +296,8 @@ class Gastos_centroCusto_CSW():
         consulta['codContaContabil'] =consulta['codContaContabil'].astype(str)
 
 
-        data2 = {'codContaContabil':['3151','3201','3323','3330'],"nomeContaContabil":["Serviços de Industrialização",'MATÉRIA PRIMA','Salarios e Ordenados','Horas extras'],
-                 "mascaraEdt":['-','-','-','-'],"GRUPO":['SERVIÇO INDUSTRIALIZACAO','MATÉRIA PRIMA','DESPESAS ADM PESSOAL','DESPESAS ADM PESSOAL']}
+        data2 = {'codContaContabil':['3151','3201','3323','3330','3411','3466'],"nomeContaContabil":["Serviços de Industrialização",'MATÉRIA PRIMA','Salarios e Ordenados','Horas extras'],
+                 "mascaraEdt":['-','-','-','-','-','-'],"GRUPO":['SERVIÇO INDUSTRIALIZACAO','MATÉRIA PRIMA','DESPESAS ADM PESSOAL','DESPESAS ADM PESSOAL','GASTOS GERAIS FABRICACAO']}
         consulta2 = pd.DataFrame(data2)
 
         consulta = pd.concat([consulta,consulta2])
