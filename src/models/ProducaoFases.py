71258434d9e531918b017d7522fa3796a95fd163
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-09-08 08:31:55 -0300

    atualizando o modulo gestao de metas pelo tipo de colecao do realizado

--- src/models/ProducaoFases.py
+++ src/models/ProducaoFases.py
@@ -64,7 +64,7 @@ class ProducaoFases():
         realizado = realizado[(realizado['filtro'] != '412|5')]
 
         realizado['codFase'] = np.where(realizado['codFase'].isin(['431', '455', '459']), '429', realizado['codFase'])
-        realizado['COLECAO'] = realizado['descricaolote'].apply(self.__tratamentoInformacaoColecao)
+        realizado['Tipo Producao'] = realizado['descricaolote'].apply(self.__tratamentoInformacaoColecao)
         df = pd.DataFrame(self.arrayTipoProducao, columns=['Tipo Producao'])
         realizado = pd.merge(realizado, df, on='Tipo Producao')
 
