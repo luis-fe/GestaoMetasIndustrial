853a3413cfdaef682ad49e9f706e5f52c578fa00
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-11-04 16:18:45 -0300

    atualizando o modulo industrial para o controle das pilotos

--- src/models/Tags_csw.py
+++ src/models/Tags_csw.py
@@ -49,8 +49,12 @@ class Tag_Csw():
 
 
         inventario = self.__ultimo_inventario_tag()
+        ultimamov = self._ultima_saida_tercerizado()
 
         consulta = pd.merge(consulta, inventario, on='codBarrasTag', how='left')
+        consulta = pd.merge(consulta, ultimamov, on='codBarrasTag', how='left')
+
+
         consulta.fillna('-',inplace=True)
 
         return consulta
@@ -89,5 +93,48 @@ class Tag_Csw():
         # Libera memória manualmente
         del rows
         gc.collect()
+
+        return consulta
+
+
+
+    def _ultima_saida_tercerizado(self):
+
+        sql = """
+        	SELECT
+                observacao1 as codbarrastag,
+                numeroOP,
+                observacao10,
+                nomeFase 
+            FROM
+                tco.RoteiroOP m
+            WHERE
+                m.codEmpresa = 1
+                and m.observacao1 like '0%'
+                and m.codFase in (406, 429, 425 )
+        """
+
+
+        with ConexaoERP.ConexaoInternoMPL() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(sql)
+                colunas = [desc[0] for desc in cursor.description]
+                rows = cursor.fetchall()
+                consulta = pd.DataFrame(rows, columns=colunas)
+
+        # Libera memória manualmente
+        del rows
+        gc.collect()
+
+        # --- Extrai data/hora, separa data e hora ---
+        consulta['dataHoraFase'] = consulta['observacao10'].str.extract(r'(\d{2}/\d{2}/\d{4}\s\d{2}:\d{2})')
+        consulta['dataHoraFase'] = pd.to_datetime(consulta['dataHoraFase'], format='%d/%m/%Y %H:%M')
+        consulta['dataFase'] = consulta['dataHoraFase'].dt.date
+        consulta['horaFase'] = consulta['dataHoraFase'].dt.time
+        consulta['observacao10'] = consulta['observacao10'].str.replace(r'\d{2}/\d{2}/\d{4}\s\d{2}:\d{2}', '',
+                                                            regex=True).str.strip()
+
+        # --- Remove duplicadas, mantendo a última movimentação ---
+        consulta = consulta.sort_values('dataHoraFase').drop_duplicates(subset='codbarrastag', keep='last').reset_index(drop=True)
 
         return consulta
